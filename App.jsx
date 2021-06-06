abcaff7adf9d55febf3c6aa7009fa7aa804448a2
yashdhanawade <85440094+yashdhanawade@users.noreply.github.com>
2021-06-06 18:50:23 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import Note1 from "./Note1";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Note />
+      <Note />
+      <Note />
+      <Note1 />
+    </div>
+  );
+}
+
+export default App;
